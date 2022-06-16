154384ac6511a7a9c0f07574e280eb3e03716877
nurlankkz2502 <99648279+nurlankkz2502@users.noreply.github.com>
2022-06-16 22:28:31 +0600

    add app_Color.dart

---
+++ lib/constants/app_colors.dart
@@ -0,0 +1,12 @@
+import 'dart:ui';
+
+class AppColors {
+  static const background = Color(0xFFE5E5E5);
+  static const txtBoxSearch = Color(0xFFF2F2F2);
+  static const mainText = Color(0xFF0B1E2D);
+  static const minorText = Color(0XFF828282);
+  static const statusText1 = Color(0xFf219653);
+  static const statusText2 = Color(0xFFEB5757);
+  static const icon = Color(0xFF5B6975);
+  static const txtBoxFrame = Color(0xFFF2F2F2);
+}
---
+++ lib/constants/app_styles.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import 'app_colors.dart';
+
+class AppStyles {
+  static const s10w500 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+    color: AppColors.mainText
+
+ )
+}
